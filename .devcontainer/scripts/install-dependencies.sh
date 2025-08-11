0f5793500846c5d9a56dc8fe11a6302e3ad5d79a
tazihad <tazihad@gmail.com>
2025-08-11 15:06:57 +0600

    fix: update install-dependencies script to clarify gem installation path

--- .devcontainer/scripts/install-dependencies.sh
+++ .devcontainer/scripts/install-dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "Installing dependencies..."
+echo "Installing gems only for user account to avoid permission issues."
 
+bundle config set --local path 'vendor/bundle'
 bundle install
 
 echo "Dependencies installed."
